b70e51a09ba8e04d4552fecf4e867772c48e9906
Shandira Ferguson <shandira.ferguson@gmail.com>
2022-10-22 17:54:16 -0700

    locations jQuery logic complete

--- scripts/locations.js
+++ scripts/locations.js
@@ -1,5 +1,13 @@
 var $locationsLink = $('.dropdown a'); 
+var $dropdownMenu = $('.dropdown');
 
-$locationsLink.click(function() {
-    $(this).toggleClass('highlight');
+$locationsLink.click(function(e) {
+    if ($dropdownMenu.hasClass("show")) {
+        $(this).removeClass('highlight');
+    } else {
+        mouseInside = false;
+        $(this).addClass('highlight');
+    }
+ }).focusout(function() {
+    $(this).removeClass('highlight');
  });
